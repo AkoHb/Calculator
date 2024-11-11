8ba89551e03dc3ed80fb4551e2eb202f21687694
AkoHb <toshick9@gmail.com>
2024-11-11 12:44:40 +0300

    Init header and footer sect

--- calculator-fcc/src/index.js
+++ calculator-fcc/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header />
     <App />
-    <div>Uincwcwhfyrhceynh egcemtgceht eot heget v</div>
+    <Footer />
   </React.StrictMode>
 );
 
